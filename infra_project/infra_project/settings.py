05f198816da1c87df2630ac19af58b713acc2fc6
Kaluga86 <nomid86@gmail.com>
2022-10-25 21:49:48 +0500

    update allowed_hosts

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -24,7 +24,12 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '*',
+    '158.160.17.35'
+    '.localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
